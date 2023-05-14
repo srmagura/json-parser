19bc6814422624fd717021ca1994fdc5d0318065
Sam Magura <srmagura@gmail.com>
2023-05-14 09:25:47 -0400

    Create Token struct

--- app/lexer.go
+++ app/lexer.go
@@ -4,71 +4,85 @@ import (
 	"regexp"
 )
 
+type TokenType byte
+
+const (
+	Number TokenType = iota
+	Boolean
+	String
+)
+
+type Token struct {
+	Type  TokenType
+	Value string
+}
+
 // The individual lex functions return an empty string if they failed to read a
 // token.
 
 var digitRegex = regexp.MustCompile("[0-9]")
 
-func lexNumber(input string, i int) string {
+func lexNumber(input string, i int) *Token {
 	token := ""
 
+	// This is going to need to be reworked to support floats
 	for j := i; j < len(input); j++ {
 		charString := input[j : j+1]
 
 		if digitRegex.MatchString(charString) {
 			token += charString
 		} else {
-			return ""
+			return nil
 		}
 	}
 
-	return token
+	return &Token{Number, token}
 }
 
-func lexBoolean(input string, i int) string {
+func lexBoolean(input string, i int) *Token {
 	if i+5 <= len(input) && input[i:i+5] == "false" {
-		return "false"
+		return &Token{Boolean, "false"}
 	}
 
 	if i+4 <= len(input) && input[i:i+4] == "true" {
-		return "true"
+		return &Token{Boolean, "true"}
 	}
 
-	return ""
+	return nil
 }
 
 // Does not currently handle escaped quotes
 // Does not currently handle newlines
-func lexString(input string, i int) string {
+func lexString(input string, i int) *Token {
 	j := i
 
 	if input[j] != '"' {
-		return ""
+		return nil
 	}
 
 	j++
 
 	for ; j < len(input); j++ {
 		if input[j] == '"' {
-			return input[i : j+1]
+			return &Token{String, input[i : j+1]}
 		}
 	}
 
 	// We got to the end without finding a closing quote
-	return ""
+	return nil
 }
 
 func Lex(input string) (tokens []string, ok bool) {
 	tokens = []string{}
 	i := 0
 
-	check := func(token string) bool {
-		if len(token) == 0 {
+	check := func(token *Token) bool {
+		if token == nil {
 			return false
 		}
 
-		tokens = append(tokens, token)
-		i += len(token)
+		tokens = append(tokens, token.Value)
+		i += len(token.Value)
 		return true
 	}
 
